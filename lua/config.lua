16f75ec4b4911ae6152cd072b1541dff317d0e4c
teiron3 <teiron@tk2-401-41583.vs.sakura.ne.jp>
2023-01-18 00:56:32 +0900

    add setting

--- lua/config.lua
+++ lua/config.lua
@@ -4,3 +4,4 @@ vim.opt.wrap = false
 local shiftwidth = 2
 vim.opt.tabstop = shiftwidth
 vim.opt.shiftwidth = shiftwidth
+vim.cmd("colorscheme darkblue")
--- lua/packersetup.lua
+++ lua/packersetup.lua
@@ -26,7 +26,3 @@ require('mason-lspconfig').setup_handlers({ function(server)
 	}
 	require('lspconfig')[server].setup(opt)
 end })
-
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-	vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
-)
